194ab27cb7a36f90edaeda08ecf828e18a92e92f
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-10-17 16:36:11 +0200

    Initial commit

---
+++ client/src/ClientApp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class ClientApp extends Component {
+  render() {
+    return (
+      <h1>ClientApp</h1>
+    );
+  }
+}
---
+++ client/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { ClientApp } from './ClientApp';
+
+React.render(<ClientApp />, document.getElementById('root'));
